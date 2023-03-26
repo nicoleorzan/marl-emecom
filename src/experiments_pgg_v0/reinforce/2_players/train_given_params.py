52e90402bc2685cc9ee1c5aa45ae632b6e65449f
Nicole <orzan.nicole@gmail.com>
2023-03-26 12:50:50 +0200

    .

--- src/experiments_pgg_v0/reinforce/2_players/train_given_params.py
+++ src/experiments_pgg_v0/reinforce/2_players/train_given_params.py
@@ -12,7 +12,7 @@ EPOCHS = 600
 OBS_SIZE = 1
 ACTION_SIZE = 2
 DECAY_RATE = 0.999
-WANDB_MODE = "offline"
+WANDB_MODE = "online"
 RANDOM_BASELINE = False
 
 # set device to cpu or cuda
@@ -171,7 +171,7 @@ def train(args, repo_name):
                     df_mex = {ag_idx+"messages_prob_distrib_m_"+str(i): mex_distrib_given_m[i][ag_idx] for i in config.mult_fact}
                     df_sc = {ag_idx+"sc": agent.sc_old[-1]}
                     df_sc_m = {ag_idx+"sc_given_m"+str(i): agent.sc_m[i][0] for i in config.mult_fact}
-                    df_ent = {ag_idx+"_avg_mex_entropy": torch.mean(agent.entropy)}
+                    df_ent = {ag_idx+"_avg_mex_entropy": np.mean(agent.buffer.comm_entropy)}
                     df_agent = {**df_agent, **df_mex, **df_sc, **df_ent, **df_sc_m}
                 if (config.listening_agents[agent.idx] == 1.):
                     df_listen = {ag_idx+"mutinfo_listening": agent.mutinfo_listening_old[-1]}
