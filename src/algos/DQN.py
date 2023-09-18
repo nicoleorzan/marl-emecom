2c557faaea1a2d91b1f4dcceb5770a619fb7eb55
Nicole <orzan.nicole@gmail.com>
2023-09-18 11:34:53 +0200

    typo in epsilon update (smaller than 0)

--- src/algos/DQN.py
+++ src/algos/DQN.py
@@ -205,7 +205,8 @@ class DQN():
     
     def update_epsilon(self, _iter):
         #self.epsilon = self.eps0*(1.-self.r)**_iter
-        self.epsilon = self.epsilon -self.epsilon_delta #self.eps0*(1.-self.r)**_iter
+        if (self.epsilon >= self.final_epsilon): 
+            self.epsilon = self.epsilon -self.epsilon_delta #self.eps0*(1.-self.r)**_iter
 
     def update_target_model(self):
         self.update_count += 1
